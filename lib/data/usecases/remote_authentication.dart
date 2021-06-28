c3a94c9ff56633e554195faf5c6ca836d691989b
João Pedro <jpedrodaibello1999@outlook.com>
2021-06-28 11:34:07 -0300

    Ensuring RemoteAuthentication throws UnexpectedError on 400

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -1,5 +1,6 @@
 import 'package:meta/meta.dart';
 
+import '../../domain/helpers/helpers.dart';
 import '../../domain/usecases/usecases.dart';
 
 import '../http/http.dart';
@@ -11,8 +12,12 @@ class RemoteAuthentication {
   RemoteAuthentication({@required this.httpClient, @required this.url});
 
   Future<void> auth(AuthenticationParams params) async {
-    final body = RemoteAuthenticationParams.fromDomain(params).toJson();
-    await httpClient.request(url: url, method: 'post', body: body);
+    try {
+      final body = RemoteAuthenticationParams.fromDomain(params).toJson();
+      await httpClient.request(url: url, method: 'post', body: body);
+    } on HttpError {
+      throw DomainError.unexpected;
+    }
   }
 }
 
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -2,6 +2,7 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:flutter_clean_architecture/domain/helpers/helpers.dart';
 import 'package:flutter_clean_architecture/domain/usecases/usecases.dart';
 
 import 'package:flutter_clean_architecture/data/usecases/usecases.dart';
@@ -23,6 +24,7 @@ void main() {
   test('Should call HttpClient with correct values', () async {
     final params = AuthenticationParams(
         email: faker.internet.email(), secret: faker.internet.password());
+
     await sut.auth(params);
 
     verify(httpClient.request(
@@ -30,5 +32,20 @@ void main() {
       method: 'post',
       body: {'email': params.email, 'password': params.secret},
     ));
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 400', () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.badRequest);
+
+    final params = AuthenticationParams(
+        email: faker.internet.email(), secret: faker.internet.password());
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
   });
 }

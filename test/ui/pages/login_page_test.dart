b1cbfe85acfd0e2c8823dfd85044bba9365adc76
João Pedro <jpedrodaibello1999@outlook.com>
2021-07-05 08:52:16 -0300

    Ensuring LoginPage calls presents no error if email is valid

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -31,7 +31,9 @@ void main() {
     await loadPage(tester);
 
     final emailTextChildren = find.descendant(
-        of: find.bySemanticsLabel('E-mail'), matching: find.byType(Text));
+      of: find.bySemanticsLabel('E-mail'),
+      matching: find.byType(Text),
+    );
     expect(
       emailTextChildren,
       findsOneWidget,
@@ -40,7 +42,9 @@ void main() {
     );
 
     final passwordTextChildren = find.descendant(
-        of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
+      of: find.bySemanticsLabel('Senha'),
+      matching: find.byType(Text),
+    );
     expect(
       passwordTextChildren,
       findsOneWidget,
@@ -73,5 +77,37 @@ void main() {
     await tester.pump();
 
     expect(find.text('any error'), findsOneWidget);
+  });
+
+  testWidgets('Should present no error if email is valid',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    emailErrorController.add(null);
+    await tester.pump();
+
+    expect(
+      find.descendant(
+        of: find.bySemanticsLabel('E-mail'),
+        matching: find.byType(Text),
+      ),
+      findsOneWidget,
+    );
+  });
+
+  testWidgets('Should present no error if email is valid',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    emailErrorController.add('');
+    await tester.pump();
+
+    expect(
+      find.descendant(
+        of: find.bySemanticsLabel('E-mail'),
+        matching: find.byType(Text),
+      ),
+      findsOneWidget,
+    );
   });
 }

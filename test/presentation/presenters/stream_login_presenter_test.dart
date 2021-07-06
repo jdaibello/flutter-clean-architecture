81359d2d1956437d22e54719f27258a47a3fe87e
João Pedro <jpedrodaibello1999@outlook.com>
2021-07-06 11:12:05 -0300

    Moving Validation to its own file

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,13 +1,10 @@
 import 'dart:async';
 
 import 'package:faker/faker.dart';
+import 'package:flutter_clean_architecture/presentation/protocols/validation.dart';
 import 'package:meta/meta.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-
-abstract class Validation {
-  String validate({@required String field, @required String value});
-}
 
 class ValidationSpy extends Mock implements Validation {}
 
